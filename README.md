dea6f357e316ee40ac772851b6588dd15daa6483
Jakob Voss <voss@gbv.de>
2014-06-30 14:09:31 +0200

    simple LOD resolver

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Linked Open Data resolver
+
+Requires Apache2 with mod_rewrite and PHP5.
+
+* `index.php`
+* `.htaccess`
+* `config.ini`
+
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+$config = 'config.ini';
+$resources = parse_ini_file($config,true);
+
+$path   = @$_REQUEST['path'];
+$format = @$_REQUEST['format']; if (!$format) $format = 'html';
+$debug  = (array_key_exists('debug',$_REQUEST) and $_REQUEST['debug'] != '0');
+
+$id = $path; # TODO: tidy up (?)
+
+if ( $res = @$resources[$id] ) {
+    $location = @$res[$format] ? @$res[$format] : 'html';
+}
+
+if ($debug) { ?>
+  <dl>
+      <dt>path</dt>
+      <dd><tt><?php echo htmlspecialchars($path) ?></tt></dd>
+      <dt>format</dt>
+      <dd><tt><?php echo htmlspecialchars($format) ?></tt></dd>
+      <dt>location</dt>
+      <dd><tt><?php echo htmlspecialchars($location) ?></tt></dd>
+  </dl>
+  <hr>
+  <b><a href="<?php print "$config\">$config"; ?></a></b>
+  <pre><?php
+    array_walk_recursive($resources, function(&$v) { 
+        $v = htmlspecialchars($v); 
+    });
+    print_r($resources);
+  ?><pre><?php 
+} else {
+    if ($location) {
+        header("HTTP/1.1 303 See Other");
+        header("Location: $location");
+    } else {
+        header("HTTP/1.1 404 Not Found");
+        echo 'Not Found';
+    }
+} ?>
